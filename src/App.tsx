53f053ffd03522bc4adb590cab027e4aa07164fd
vinod <vinodkotagiri@icloud.com>
2024-10-25 14:29:08 +0530

    added data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarComponent from './components/NavbarComponent'
 import ScheduleComponent from './components/ScheduleComponent'
 import GanttChartComponent from './components/GanttChartComponent'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarDays, faChartGantt } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays, faChartGantt, faMoneyBillTrendUp } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
+  const [currentView, setCurrentView] = useState<number>(0);
+  function handleViewChange(view: number) {
+    setCurrentView(view)
+  }
   return (<div className='h-screen min-w-screen overflow-hidden'>
     <div className='sticky top-0 left-0 z-50'>
       <NavbarComponent />
@@ -13,26 +17,29 @@ const App = () => {
 
     <ul className="menu bg-base-200 lg:menu-horizontal rounded-box w-full">
       <li className='flex items-center justify-center'>
-        <button className='flex items-center justify-center'>
-        <FontAwesomeIcon icon={faCalendarDays} />
-          Inbox
+        <button className='flex items-center justify-center' onClick={() => handleViewChange(0)}>
+          <FontAwesomeIcon icon={faCalendarDays} />
+          Schedule
         </button>
       </li>
+      <div className="divider divider-horizontal"></div>
       <li>
-        <a>
-    <FontAwesomeIcon icon={faChartGantt}/>
-          Updates
-        </a>
+        <button className='flex items-center justify-center' onClick={() => handleViewChange(1)}>
+          <FontAwesomeIcon icon={faChartGantt} />
+          Gantt Chart
+        </button>
       </li>
+      <div className="divider divider-horizontal"></div>
       <li>
-        <a>
-          Stats
-
-        </a>
+        <button className='flex items-center justify-center' onClick={() => handleViewChange(2)}>
+          <FontAwesomeIcon icon={faMoneyBillTrendUp} />
+          Estimation
+        </button>
       </li>
     </ul>
 
-
+    {currentView === 0 && <ScheduleComponent />}
+    {currentView === 1 && <GanttChartComponent />}
 
   </div>
   )
