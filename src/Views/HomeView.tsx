c32f1deda987df9ae173c4aa5645658e4253103d
vinod kotagiri <vinodkotagiri@icloud.com>
2024-11-02 21:04:39 +0530

    made responsive charts

--- src/Views/HomeView.tsx
+++ src/Views/HomeView.tsx
@@ -1,42 +1,51 @@
-import ChartComponent from "../components/ChartComponent"
-import { LineChart, PieChart } from "../constants/charts"
+import ChartComponent from "../components/ChartComponent";
+import { LineChart, PieChart } from "../constants/charts";
 
 const HomeView = () => {
   return (
-    <div className="w-screen h-[calc(100vh-64px)] bg-slate-900 px-4 py-1">
-      <div className="w-full h-[120px]  flex gap-3 px-3 py-1">
-        <div className="w-1/6 h-full rounded-md shadow-slate-600 bg-[#F55C47]">HELLO</div>
-        <div className="w-1/6 h-full rounded-md bg-[#9FE6A0]">HELLO</div>
-        <div className="w-1/6 h-full rounded-md bg-[#4AA96C]">HELLO</div>
-        <div className="w-1/6 h-full rounded-md  bg-[#564A4A]">HELLO</div>
-        <div className="w-1/6 h-full rounded-md bg-[#B31312]">HELLO</div>
-        <div className="w-1/6 h-full rounded-md bg-[#EA906C]">HELLO</div>
-      </div>
-      <div className="w-full flex flex-col h-[calc(100%-120px)] p-3 gap-2">
-        <div className="w-full h-1/2 flex items-center gap-2">
-          <div className="w-1/4 h-full">
-         <ChartComponent chart={LineChart}/> 
-          </div>
-          <div className="w-1/4 h-full">
-          <ChartComponent chart={PieChart}/> 
-          </div>
-          <div className="w-1/4 h-full"></div>
-          <div className="w-1/4 h-full"></div>
+    <div className="w-screen h-[calc(100vh-64px)] bg-slate-900 px-4 py-1 overflow-y-auto">
+      {/* Top Section */}
+      <div className="w-full flex flex-wrap gap-3 p-3 justify-center">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 min-h-[80px] rounded-md shadow-md bg-[#F55C47] flex items-center justify-center">
+          HELLO
         </div>
-        <div className="w-full h-1/2  flex items-center gap-2">
-     <div className="w-1/3 h-full"></div>
-     <div className="w-1/3 h-full"></div>
-     <div className="w-1/3 h-full"></div>
-   </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 min-h-[80px] rounded-md shadow-md bg-[#9FE6A0] flex items-center justify-center">
+          HELLO
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 min-h-[80px] rounded-md shadow-md bg-[#4AA96C] flex items-center justify-center">
+          HELLO
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 min-h-[80px] rounded-md shadow-md bg-[#564A4A] flex items-center justify-center">
+          HELLO
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 min-h-[80px] rounded-md shadow-md bg-[#B31312] flex items-center justify-center">
+          HELLO
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="w-full flex flex-col h-[calc(100%-120px)] p-3 gap-2">
+        {/* Chart Row */}
+        <div className="w-full h-1/2 flex flex-wrap items-center gap-2">
+          <div className="w-full sm:w-1/2 lg:w-1/4 h-full">
+            <ChartComponent chart={LineChart} />
+          </div>
+          <div className="w-full sm:w-1/2 lg:w-1/4 h-full">
+            <ChartComponent chart={PieChart} />
+          </div>
+          <div className="w-full sm:w-1/2 lg:w-1/4 h-full"></div>
+          <div className="w-full sm:w-1/2 lg:w-1/4 h-full"></div>
+        </div>
+
+        {/* Empty Content Row */}
+        <div className="w-full h-1/2 flex flex-wrap items-center gap-2">
+          <div className="w-full sm:w-1/2 lg:w-1/3 h-full"></div>
+          <div className="w-full sm:w-1/2 lg:w-1/3 h-full"></div>
+          <div className="w-full sm:w-1/2 lg:w-1/3 h-full"></div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeView
-
-
-
-
-
-
+export default HomeView;
