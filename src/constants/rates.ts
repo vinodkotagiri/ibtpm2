c490a3fc51f3380bd634e28beacfe0c3600848e7
Rajesh Uba <rajeshuba1@gmail.com>
2024-11-04 11:47:13 +0530

    schedule added tasks, rates and types

--- src/constants/rates.ts
+++ src/constants/rates.ts
@@ -1,29 +1,31 @@
 export const MasonRates = {
-  Daily: 1,
-  Weekly: 10,
-  Monthly: 100,
-  Contract: 150,
-  Hourly: 0.25,
-  Sqft: 0.05,
-  Cum: 0.8
+  Mason:{
+  Daily: 1400,
+  Weekly: 1400*6,
+  Monthly: 1400*24,
+  Contract: 100,
+  Hourly: 150,
+  Sqft: 350,
+  Cum: 350
 }
-
+}
 export const HelperRates = {
-  Daily: 1,
-  Weekly: 10,
-  Monthly: 100,
-  Contract: 150,
-  Hourly: 0.25,
-  Sqft: 0.05,
-  Cum: 0.8,
-  MT: 1.2
+  Helper:{
+  Daily: 1000,
+  Weekly: 1000*6,
+  Monthly: 1000*24,
+  Contract: 100,
+  Hourly: 150,
+  Sqft: 350,
+  Cum: 350,
+  MT: 350
 }
-
+}
 export const MachinaryRates = {
   Excavator:{
-    Daily: 1,
-    Weekly: 10,
-    Monthly: 100,
+    Daily: 1250,
+    Weekly: 8500,
+    Monthly: 21000,
     Contract: 150,
     Hourly: 0.25,
     Sqft: 0.05,
@@ -43,7 +45,7 @@ export const MachinaryRates = {
 }
 
 export const BarBenderRates = {
-  Daily: 1,
+  Daily: 1200,
   Weekly: 10,
   Monthly: 100,
   Contract: 150,
@@ -51,4 +53,47 @@ export const BarBenderRates = {
   Sqft: 0.05,
   Cum: 0.8,
   MT: 1.2
+}
+
+export const ConcreteRates = {
+  M10pcc:{
+    Sqft: 0.05,
+    Cum: 4000,
+    MT: 1.2
+  },
+  M15pcc:{    
+    Sqft: 0.05,
+    Cum: 4000,
+    MT: 1.2
+  },
+  M20pcc:{    
+    Sqft: 0.05,
+    Cum: 4500,
+    MT: 1.2
+  },
+  M25pcc:{    
+    Sqft: 0.05,
+    Cum: 5000,
+    MT: 1.2
+  },
+  M30pcc:{    
+    Sqft: 0.05,
+    Cum: 5500,
+    MT: 1.2
+  },
+  M35pcc:{    
+    Sqft: 0.05,
+    Cum: 6000,
+    MT: 1.2
+  },
+  M40pcc:{    
+    Sqft: 0.05,
+    Cum: 6500,
+    MT: 1.2
+  },
+  M45pcc:{    
+    Sqft: 0.05,
+    Cum: 7000,
+    MT: 1.2
+  },
 }
